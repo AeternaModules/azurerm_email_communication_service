4946939a78cf3560121591b1c67190cdc4a067f2
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:27 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "email_communication_services" {
-  description = "All email_communication_service resources"
-  value       = azurerm_email_communication_service.email_communication_services
-}
 output "email_communication_services_data_location" {
-  description = "List of data_location values across all email_communication_services"
-  value       = [for k, v in azurerm_email_communication_service.email_communication_services : v.data_location]
+  description = "Map of data_location values across all email_communication_services, keyed the same as var.email_communication_services"
+  value       = { for k, v in azurerm_email_communication_service.email_communication_services : k => v.data_location }
 }
 output "email_communication_services_name" {
-  description = "List of name values across all email_communication_services"
-  value       = [for k, v in azurerm_email_communication_service.email_communication_services : v.name]
+  description = "Map of name values across all email_communication_services, keyed the same as var.email_communication_services"
+  value       = { for k, v in azurerm_email_communication_service.email_communication_services : k => v.name }
 }
 output "email_communication_services_resource_group_name" {
-  description = "List of resource_group_name values across all email_communication_services"
-  value       = [for k, v in azurerm_email_communication_service.email_communication_services : v.resource_group_name]
+  description = "Map of resource_group_name values across all email_communication_services, keyed the same as var.email_communication_services"
+  value       = { for k, v in azurerm_email_communication_service.email_communication_services : k => v.resource_group_name }
 }
 output "email_communication_services_tags" {
-  description = "List of tags values across all email_communication_services"
-  value       = [for k, v in azurerm_email_communication_service.email_communication_services : v.tags]
+  description = "Map of tags values across all email_communication_services, keyed the same as var.email_communication_services"
+  value       = { for k, v in azurerm_email_communication_service.email_communication_services : k => v.tags }
 }
 
