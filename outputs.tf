3b0f3c266b6efc8dd2d1c88fb0d5138dc7dacbec
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:02:05 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "email_communication_services_id" {
+  description = "Map of id values across all email_communication_services, keyed the same as var.email_communication_services"
+  value       = { for k, v in azurerm_email_communication_service.email_communication_services : k => v.id }
+}
 output "email_communication_services_data_location" {
   description = "Map of data_location values across all email_communication_services, keyed the same as var.email_communication_services"
   value       = { for k, v in azurerm_email_communication_service.email_communication_services : k => v.data_location }
